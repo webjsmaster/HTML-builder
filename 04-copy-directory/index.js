21832f30eee831921846bccaab052f4de200a825
webjsmaster <web.js.master@gmail.com>
2023-05-07 21:18:37 +0300

    feat:04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require("fs");
+const { copyFile, mkdir } = require("fs/promises");
+
+async function copyDir () {
+    const newDir = __dirname + "/files-copy";
+    const dir = __dirname + "/files";
+
+
+    fs.readdir( dir, function ( err, files) {
+        if (err || !files.length) {
+            throw new Error ('FS operation failed');
+        } else {
+            ensureDirSync(newDir, files);
+        }
+    })
+
+    async function ensureDirSync (folder, files) {
+        try {
+            await mkdir(folder);
+            await Promise.all(files.map( file  => copyFile (dir + '/' + file, newDir + '/' + file)));
+            console.log(`\x1b[33mSuccess!\x1b[0m`);
+        } catch (err) {
+            await Promise.all(files.map( file  => copyFile (dir + '/' + file, newDir + '/' + file)));
+            console.log(`\x1b[33mSuccess!\x1b[0m`);
+        }
+    }  
+}
+
+copyDir();
+
+
+
 
